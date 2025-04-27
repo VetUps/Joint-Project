3a8ecc71310b19efc35ebba336ee4943b01a3abe
Никита Чернов <sercherufa@gmail.com>
2025-04-27 22:43:37 +0500

    Разработан TabelControl для отображения столиков

---
+++ Restaurant/Restaurant/Views/UserControls/TableControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restaurant.Views.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для TableControl.xaml
+    /// </summary>
+    public partial class TableControl : UserControl
+    {
+        public TableControl(Table table)
+        {
+            InitializeComponent();
+            DataContext = table;
+
+            StatusColor();
+        }
+
+        private void StatusColor()
+        {
+            if (tableStatusTextBlock.Text == "Свободен")
+                tableStatusTextBlock.Foreground = new SolidColorBrush(Colors.Green);
+
+            else if (tableStatusTextBlock.Text == "Занят")
+                tableStatusTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+
+            else if (tableStatusTextBlock.Text == "Зарезервирован")
+                tableStatusTextBlock.Foreground = new SolidColorBrush(Colors.Yellow);
+        }
+    }
+}
