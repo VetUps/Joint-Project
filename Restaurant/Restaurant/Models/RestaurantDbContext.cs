0560cbabf1da76b843a13a05fd5ee88885a5679b
Никита Чернов <sercherufa@gmail.com>
2025-05-28 11:16:05 +0500

    Смена строки подключения

--- Restaurant/Restaurant/Models/RestaurantDbContext.cs
+++ Restaurant/Restaurant/Models/RestaurantDbContext.cs
@@ -38,7 +38,7 @@ public partial class RestaurantDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=restaurant_db;user=root;password=1234", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
+        => optionsBuilder.UseMySql("server=176.113.83.11;database=restaurant_db;user=mega_user;password=1234", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
