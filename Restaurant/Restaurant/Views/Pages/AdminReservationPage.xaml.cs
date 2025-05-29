7c56d2cce080aae8b1ac8d83dcf53193e0f4b8bf
Никита Чернов <sercherufa@gmail.com>
2025-05-29 13:59:38 +0500

    Улучшение внешнего вида списка броней, реализован вывод списка

--- Restaurant/Restaurant/Views/Pages/AdminReservationPage.xaml.cs
+++ Restaurant/Restaurant/Views/Pages/AdminReservationPage.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.Models;
+using Restaurant.Views.UserControls;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +18,67 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static Restaurant.Views.Pages.OrdersAdminPage;
 
 namespace Restaurant.Views.Pages
 {
     /// <summary>
     /// Логика взаимодействия для AdminReservationPage.xaml
     /// </summary>
-    public partial class AdminReservationPage : Page
+    public partial class AdminReservationPage : Page, INotifyPropertyChanged
     {
         public AdminReservationPage()
         {
             InitializeComponent();
+
+            DataContext = this;
+            LoadReservations();
+        }
+
+        // Реализуем ивент для обновления списка броней
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Функция для сообщения об изменениях
+        protected void OnPropertyChanged([CallerMemberName] string name = null!)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        // ObservableCollection заменит List, т.к. ListBox будет понимать, что в свойстве произошли изменения
+        private ObservableCollection<ReservationControl> _reservationCards = new();
+        public ObservableCollection<ReservationControl> ReservationCardSource
+        {
+            get => _reservationCards;
+            set
+            {
+                _reservationCards = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void LoadReservations()
+        {
+            try
+            {
+                using (var context = new RestaurantDbContext())
+                {
+                    var reservations = context.ClientTables
+                        .Include(o => o.Client)
+                        .Include(o => o.Table)
+                    .ToList();
+
+                    ReservationCardSource.Clear();
+
+                    foreach (var reservation in reservations)
+                    {
+                        ReservationCardSource.Add(new ReservationControl(reservation));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки: {ex.Message}");
+            }
         }
     }
 }
